b3e0638ea568fb4c5bd319e8956ea72f6016aa24
MAI-SR <mai.studentrobotics@gmail.com>
2018-04-09 16:40:32 +0200

    Add files via upload
    merging low level and high level code;
    release of beacon tower code

---
+++ towerMaster.ino
@@ -0,0 +1,85 @@
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#define echoPin 3
+#define trigPin 4
+
+const unsigned short towerID = 1;
+
+RF24 radio (7, 8);
+const uint64_t adress = 0xF0F0F0F0F0;
+
+
+void setup() {
+  Serial.begin(9600);
+  
+  radio.begin();
+  radio.setPALevel(RF24_PA_MAX);//tmp => cus of power reasons, should be changed to high later to strengthen signal
+  radio.setDataRate(RF24_1MBPS);//test RF24_250KBPS later maybe, but i dunno (only works with + variants), this method return a boolean that indicates the success of the setFunktion
+  radio.setAutoAck(false);//cus we want >1 devices to listen to this message... think about it a bit, it will make sence
+  radio.disableCRC();//maybe this line is not needed, but i dunno
+
+  radioWriteMode();
+
+  Serial.print("Transmitter: ");
+  Serial.print("Radio is ");
+  Serial.print(radio.isPVariant());
+  Serial.println(".");
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  digitalWrite(trigPin, LOW);
+}
+
+void prepUS(){
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+}
+
+void radioListenMode(){
+  radio.openReadingPipe(0, adress);
+  radio.startListening();
+}
+
+void radioWriteMode(){
+  radio.stopListening();
+  radio.openWritingPipe(adress);
+}
+
+void trigger()
+{
+  radioWriteMode();
+  const char message[32] = {'0', 'm', '0' + towerID};
+  radio.write(&message, sizeof(message));
+  
+  prepUS();
+  radioListenMode();
+  Serial.println("Sent out.");
+}
+
+/*
+ * Triggers another tower
+ * @param true: tower2, false: tower3
+ */
+
+void orderTrigger(bool toTrigger)
+{
+  radioWriteMode();
+  const char message[32] = {toTrigger ? '0' : '2', 't', '0' + towerID};
+  radio.write(&message, sizeof(message));
+
+  radioListenMode();
+  Serial.println("Sent out order.");
+}
+
+void loop() {
+  trigger();
+  delay(210);
+  orderTrigger(true);
+  delay(220);
+  orderTrigger(false);
+  delay(220);
+}
---
+++ towerSlave.ino
@@ -0,0 +1,86 @@
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#define echoPin 3
+#define trigPin 4
+
+const unsigned short towerID = 0;
+
+RF24 radio (7, 8);
+const uint64_t adress = 0xF0F0F0F0F0;
+
+void setup() {
+  Serial.begin(9600);
+  
+  radio.begin();
+  radio.setPALevel(RF24_PA_MAX);//tmp => cus of power reasons, should be changed to high later to strengthen signal
+  radio.setDataRate(RF24_1MBPS);//test RF24_250KBPS later maybe, but i dunno (only works with + variants), this method return a boolean that indicates the success of the setFunktion
+  radio.setAutoAck(false);//cus we want >1 devices to listen to this message... think about it a bit, it will make sence
+  radio.disableCRC();//maybe this line is not needed, but i dunno
+
+
+  Serial.print("Transmitter: ");
+  Serial.print("Radio is ");
+  Serial.print(radio.isPVariant());
+  Serial.println(".");
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  digitalWrite(trigPin, LOW);
+
+  radio.maskIRQ(1, 1, 0);//only interrupt in receiving data
+  attachInterrupt(digitalPinToInterrupt(2), receivedMessage, FALLING);
+  radioListenMode();
+}
+
+void prepUS(){
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+}
+
+void radioListenMode(){
+  radio.openReadingPipe(0, adress);
+  radio.startListening();
+}
+
+void radioWriteMode(){
+  radio.stopListening();
+  radio.openWritingPipe(adress);
+}
+
+void trigger()
+{
+  radioWriteMode();
+  const char message[32] = {'0', 'm', '0' + towerID};
+  radio.write(&message, sizeof(message));
+  
+  prepUS();
+  radioListenMode();
+  Serial.println("Send out.");
+}
+
+void receivedMessage()
+{
+
+  if(radio.available()) {
+  char text[32] = "";
+    
+  radio.read(&text, sizeof(text));
+
+  char receiver = text[0];
+    
+  if(receiver != '0' && receiver != '0' + towerID) return;
+
+  if(text[1] == 't')
+    Serial.println("Order received");
+    trigger();
+  }
+}
+
+void loop()
+{
+
+}
